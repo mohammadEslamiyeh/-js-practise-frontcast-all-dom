377ba1443de40e35fb82590c2faab3889af731dd
mohammad <mohammades6634@yahoo.com>
2020-03-24 21:24:53 +0430

    final project dom 'exercise'

--- js/main.js
+++ js/main.js
@@ -1,70 +1,49 @@
-/////////////////////////// dom 1 /////////////////////////////////
-console.log("dom 1 : find tag by id and class ,............");
-let show1, show2, show3;
+function addItem(){
+    var inputdata;
 
-show1 = document .getElementById("app");
-console.log(show1);
-show2 = document.getElementsByClassName("box");
-console.log(show2[0]);
-show3 = document.getElementsByTagName("p");
-console.log(show3[2]);
-/////////////////////////// dom 2 /////////////////////////////////
-console.log("dom 2 : chanage p text tag ");
-function change() {
-    document.getElementsByTagName("p")[4].innerHTML = "mohammad eslamiyeh";
-}
-/////////////////////////// dom 3 /////////////////////////////////
-console.log("dom 3 :find div -> change text tag ->set link and change link ");
+    inputdata = document.getElementById("input").value;
 
-let divmain;
+    if (inputdata== ""){
+        alert("لطفا کار خود را وارد کنید");
+    }else{
+        document.getElementById("input").value = "";
+        let ptag = document.createElement("p");
+        let txt = document.createTextNode(inputdata);
+        let span = document.createElement("span");
+        span.appendChild(txt);
+        let resualt = document.getElementById("result");
+        ptag.appendChild(span);
+        resualt.appendChild(ptag);
 
-function changeText(){
-    // find div
-    divmain = document.getElementById("mainDom3");
-    // change tag text
-    divmain.getElementsByTagName("p")[0].innerHTML="is new content";
-    // set link to tag
-    divmain.getElementsByTagName("a")[0].href = "https://github.com/mohammadEslamiyeh?tab=repositories";
-    divmain.getElementsByTagName("a")[1].setAttribute("href","https://vuejs.org/");
-    divmain.getElementsByTagName("p")[1].style.color="red";
-}
-/////////////////////////// dom 4 /////////////////////////////////
-console.log("dom 4 : add tag p , creat text and insert div tag ");
+        var span2 = document.createElement("span");
+        var rmtxt = document.createTextNode("X");
+        span2.appendChild(rmtxt);
+        ptag.appendChild(span2);
+        span2.style.color="red";
+        ptag.addEventListener("click",taskdone);
 
-var divtag,ptag,txt;
-divtag = document.getElementById("show");
-ptag = document.createElement("p");
-txt = document.createTextNode("my name is mohammad");
+        function taskdone(){
+            /*baraye remove kardan rahaye ziadi bod vali ma amadim az tavabe java script estefade kardim*/
+            span.style.textDecoration  = "line-through";   
+        }
+            /** 
+             * javascript comment 
+             * @Author: flydreame 
+             * @Date: 2020-03-24 21:03:09 
+             * @Desc: barayte yek moteghayeri attribute onclick tarif kardim 
+             */
+            var remove= document.createAttribute("onclick");
+            /** 
+             * javascript comment 
+             * @Author: flydreame 
+             * @Date: 2020-03-24 21:03:53 
+             * @Desc: va be on moteghayer yek value dadim va value in meghdaresh in bashe
+             * miad parent node yanoi oparent aslio az bein mibare 
+             */
+            remove.value = "this.parentNode.remove();";
+            //va inja migim key remove beshe
+            span2.setAttributeNode(remove);
 
-
-ptag.appendChild(txt);
-divtag.appendChild(ptag);
-
-/////////////////////////// dom 5 /////////////////////////////////
-console.log("dom 5 : Definition EVENT FOR TAG and get date on system");
-let p1,p2;
-
-p1 = document.getElementById("color");
-
-p1.addEventListener("click",changeColor);
-
-function changeColor(){
-    p1.style.color= "red";
-}
-p2 = document.getElementById("date");
-p2.addEventListener("click", showdate);
-
-function showdate(){
-    console.log(Date());
-}
-
-/////////////////////////// dom 6/////////////////////////////////
-console.log("dom 6 : set color for text by querySelectorAll and 'for loop'  ");
-
-let plist,divdom6;
-divdom6 = document.getElementById("dom6");
-plist = divdom6.querySelectorAll("p");
-console.log(plist[2]);
-for (i=0;i<plist.length;i++){
-plist[i].style.color="blue";
+        
+    }
 }
